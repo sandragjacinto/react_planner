30e5f2517d43d5ce0029a873aaaa8df7f6d10206
Smouska <sandragjacinto@gmail.com>
2017-07-25 17:20:35 -0400

    commits profile page layout

--- src/App.js
+++ src/App.js
@@ -15,17 +15,17 @@ class App extends Component {
         <div className="row">
 
 
-          <div className="col-md-2" style={{ textAlign: "center", background:"#64A9CF"}}>
+          <div className="col-md-2 col-xs-2" style={{ textAlign: "center", background:"#64A9CF"}}>
             <br /> <br />
             <ul style={{ paddingLeft: '30px', }} className="nav nav-pills nav-stacks">
-              <a href={'/profile'} style={{ color:'white' }}><img className="img-responsive" src={profile} alt="logo" />Profile</a>
-              <a href={'/homepage'} style={{ color:'white' }}><img className="img-responsive" src={logo} alt="logo" />Home Page</a>
-              <a href={'/choosemymeal'} style={{ color:'white' }}><img className="img-responsive" src={CMMimage} alt="logo" />Choose My Meal</a>
-              <a href={'/cookmymeal'} style={{ color:'white' }}><img className="img-responsive" src={cooking} alt="logo" />Cook My Meal</a>
-              <a href={'/grocerylist'} style={{ color:'white' }}><img className="img-responsive" src={grocery} alt="logo" />Grocery List</a>
+              <a href={'/profile'} style={{ color:'white' }}><img className="img-responsive" src={profile} alt="logo"  style={{ maxWidth: '50%', maxHeight:'50%' }}/>Profile</a>
+              <a href={'/homepage'} style={{ color:'white' }}><img className="img-responsive" src={logo} alt="logo" style={{ maxWidth: '50%', maxHeight:'50%' }}/>Home Page</a>
+              <a href={'/choosemymeal'} style={{ color:'white' }}><img className="img-responsive" src={CMMimage} alt="logo" style={{ maxWidth: '50%', maxHeight:'50%' }}/>Choose My Meal</a>
+              <a href={'/cookmymeal'} style={{ color:'white' }}><img className="img-responsive" src={cooking} alt="logo" style={{ maxWidth: '50%', maxHeight:'50%' }}/>Cook My Meal</a>
+              <a href={'/grocerylist'} style={{ color:'white' }}><img className="img-responsive" src={grocery} alt="logo" style={{ maxWidth: '50%', maxHeight:'50%' }}/>Grocery List</a>
             </ul>
           </div>
-          <div className="col-md-10" style={{ paddingTop: '30px', }}>
+          <div className="col-md-10 col-xs-10" style={{ paddingTop: '30px', }}>
 
         {this.props.children}
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,30 +1,88 @@
 import React from 'react';
 
 
-/*const SearchInput =  (props ) => {
-    return  (
-            <input type='text' onChange={this.props.onChangeSearchInput} placeholder='Recipe Name'> </input>
+const profileIMG = 'http://en.cubadebate.cu/files/2012/10/chavez3.jpg';
+const userID = {
+    userName: 'Chavez2017',
+    userEmail: 'yosoychavez2017@gmail.com',
+    tutorName: 'Pedro Pena'
+}
+
+const UserName = (userinfo) => {
+    return (
+        <div>
+            <div className='row'>
+                    <div className='col-md-3 col-xs-3' style={{textAlign:'right'}}>
+                        <p> User Name:</p>
+                    </div>
+                    <div className='col-md-4 col-xs-4' style={{textAlign:'left'}}>
+                        <p>{userinfo.userinfo.userName}</p>
+                    </div>
+            </div>
+            <div className='row'>
+                <div className='col-md-3 col-xs-3' style={{textAlign:'right'}}>
+                    <p>  Email :</p>
+                </div>
+                <div className='col-md-4 col-xs-4' style={{textAlign:'left'}}>
+                    <p>{userinfo.userinfo.userEmail}</p>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-md-3 col-xs-3' style={{textAlign:'right'}}>
+                    <p> Tutor Name :</p>
+                </div>
+                <div className='col-md-4 col-xs-4' style={{textAlign:'left'}}>
+                    <p>{userinfo.userinfo.tutorName}</p>
+                </div>
+            </div>
+        </div>
+
     )
-}*/
+}
 
-//SearchButton
+const LoadProfileIm =(profileIMG) =>{
+return(
+<img alt='profilepic' src={profileIMG.profileIMG} style={{ maxWidth: '100%', maxHeight:'100%' }} />
+)}
 
-//SearchResultList
+class Profile extends React.Component {
 
-//AcceptButton
-
-
-class Profile extends React.Component  {
-   
-
-    render()
-    {
-        return  (
+    render() {
+        return (
             <div>
-                <h2>Profile</h2>
-            </div> 
+                <div className='jumbotron'>
+                    <div className='row'>
+                        <div className='col-md-5 col-xs-5'>
+                            <h2>Profile</h2>
+                            <LoadProfileIm profileIMG={profileIMG} />
+                        </div>
+                        <div className='col-md-7 col-xs-7'>
+                            <h3>Personal Info</h3>
+                            <UserName userinfo={userID} />
+                        </div>
+                    </div>
+                </div>
+                <div className='card'>
+                    <div className='card-block'>
+                <div className='row'>
+                    <div className='col-md-4 col-xs-4'>
+                        <button type='button' className='btn btn-danger btn-lg'>I don't like</button>
+                        </div>
+                        <div className='col-md-4 col-xs-4'>
+                        <button type='button' className="btn btn-warning btn-lg">I'm alergic</button>
+                        </div>
+                        <div className='col-md-4 col-xs-4'>
+                        <button type='button' className="btn btn-primary btn-lg">I can not eat</button>
+                        </div>
+                </div>
+                </div>
+                </div>
+                <div className='jumbotron'>
+                    <h3> App Settings </h3>
+                    </div>
+            </div>
         )
     }
-} 
+}
 
 export default Profile;
