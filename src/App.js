9c912910d94adb47545f49950a0943a63a766031
Smouska <sandragjacinto@gmail.com>
2017-07-28 00:07:03 -0400

    adds i dont like feature

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
               <a href={'/choosemymeal'} style={{ color:'white' }}><img className="img-responsive" src={CMMimage} alt="logo" /></a>
               <a href={'/cookmymeal'} style={{ color:'white' }}><img className="img-responsive" src={cooking} alt="logo" /></a>
               <a href={'/grocerylist'} style={{ color:'white' }}><img className="img-responsive" src={grocery} alt="logo" /></a>
+              <a href={'/mealplanning'} style={{ color:'white' }}><img className="img-responsive" src={grocery} alt="logo" /></a>
             </ul>
           </div>
           <div className="col-md-11 col-xs-12" style={{ paddingTop: '30px', }}>
