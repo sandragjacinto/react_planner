f27283eddddd08e6d5cd78fe92c19201d6db3a71
Smouska <sandragjacinto@gmail.com>
2017-08-01 15:29:43 -0400

    makes sure bootstrap grid is in place with new menu changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -20,33 +20,38 @@ import grocery from './icons/cart.png';
 import { Link, Route, IndexRoute, BrowserRouter as Router } from 'react-router-dom';
 
 
-const router=(
+const router = (
 
     <Router>
-    <App>
-    <div style={{ textAlign: "center" }}>
-        <ul style={{ paddingLeft: '30px', }} className="nav nav-pills nav-stacks">
-        <Link to={'/profile'} style={{ color: 'white' }}><img className="img-responsive" src={profile} alt="logo" /></Link>
-        <Link to={'/homepage'} style={{ color: 'white' }}><img className="img-responsive" src={logo} alt="logo" /></Link>
-        <Link to={'/choosemymeal'} style={{ color: 'white' }}><img className="img-responsive" src={CMMimage} alt="logo" /></Link>
-        <Link to={'/cookmymeal'} style={{ color: 'white' }}><img className="img-responsive" src={cooking} alt="logo" /></Link>
-        <Link to={'/grocerylist'} style={{ color: 'white' }}><img className="img-responsive" src={grocery} alt="logo" /></Link>
-        <Link to={'/mealplanning'} style={{ color: 'white' }}><img className="img-responsive" src={grocery} alt="logo" /></Link>
-    </ul>
-            
-              <Route exact path="/" component={HomePage}></Route>
-              <Route exact path="/homepage" component={HomePage}></Route>
-              <Route path="/choosemymeal" component={ChooseMyMeal}></Route>
-              <Route path="/cookmymeal" component={CookMyMeal} ></Route>
-              <Route path="/profile" component={Profile} ></Route>
-              <Route path="/grocerylist" component={GroceryList} ></Route>
-              <Route path="/mealplanning" component={MealPlanning} ></Route>
-              <Route path="/dontlike" component={DontLike}></Route>
-            
-        </div>
+        <App>
+            <div>
+                <div className='row'>
+                <div style={{ textAlign: "center" }} className='col-md-2 col-xs-12'>
+                    <ul style={{ paddingLeft: '30px', }} className="nav nav-pills nav-stacks">
+                        <Link to={'/profile'} style={{ color: 'white' }}><img className="img-responsive" src={profile} alt="logo" /></Link>
+                        <Link to={'/homepage'} style={{ color: 'white' }}><img className="img-responsive" src={logo} alt="logo" /></Link>
+                        <Link to={'/choosemymeal'} style={{ color: 'white' }}><img className="img-responsive" src={CMMimage} alt="logo" /></Link>
+                        <Link to={'/cookmymeal'} style={{ color: 'white' }}><img className="img-responsive" src={cooking} alt="logo" /></Link>
+                        <Link to={'/grocerylist'} style={{ color: 'white' }}><img className="img-responsive" src={grocery} alt="logo" /></Link>
+                        <Link to={'/mealplanning'} style={{ color: 'white' }}><img className="img-responsive" src={grocery} alt="logo" /></Link>
+                    </ul>
+                </div>
+                
+                <div className='col-md-10 col-xs-12'>
+                <Route exact path="/" component={HomePage}></Route>
+                <Route exact path="/homepage" component={HomePage}></Route>
+                <Route path="/choosemymeal" component={ChooseMyMeal}></Route>
+                <Route path="/cookmymeal" component={CookMyMeal} ></Route>
+                <Route path="/profile" component={Profile} ></Route>
+                <Route path="/grocerylist" component={GroceryList} ></Route>
+                <Route path="/mealplanning" component={MealPlanning} ></Route>
+                <Route path="/dontlike" component={DontLike}></Route>
+                </div>
+            </div>
+            </div>
         </App>
     </Router>
-    )
-    render(router, document.getElementById('root'));
-    registerServiceWorker();
+)
+render(router, document.getElementById('root'));
+registerServiceWorker();
 
