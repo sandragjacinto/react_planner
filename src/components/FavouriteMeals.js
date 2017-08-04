6612333818aab7d94d38c5fd8e06a775e823d247
Smouska <sandragjacinto@gmail.com>
2017-08-04 12:47:07 -0400

    adds other pages for restrictions

--- src/components/FavouriteMeals.js
+++ src/components/FavouriteMeals.js
@@ -23,7 +23,7 @@ const IngredientInput = (props) => {
 const IgredientListComp = (props) => (
     <div className='row'>
         <div className="col-md-6 col-md-offset-3 col-xs-8 col-xs-offset-2 ingredientsList">
-            {props.listDontLike.map((ing, index) => {
+            {props.listFavoutiteMeals.map((ing, index) => {
                 return (
                     <div className='row list-group-item '>
                         <div className='col-md-9 col-xs-9'>
@@ -50,55 +50,57 @@ const SaveButton = (props) => (
     </div>
 )
 
-class DontLike extends React.Component {
+class FavouriteMeals extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            newIngredientDontLike: '',
-            listDontLike: [],
+            newFavouriteMeals: '',
+            listFavoutiteMeals: [],
             ItemIndex: '',
-            listDelDontLike: '',
-            dontlike: [],
+            listDelFavouriteMeal: '',
+            favouriteMeals: [],
+            
         }
     }
 
     onChangIngrdientInput = (e) => {
-        this.setState({ newIngredientDontLike: e.target.value });
-        //    console.log(this.state.newIngredientDontLike);
+        this.setState({ newFavouriteMeals: e.target.value });
+        //    console.log(this.state.newFavouriteMeals);
     }
 
     onClickAddIngredient = () => {
 
-        var listDontLike = this.state.listDontLike.concat(this.state.newIngredientDontLike)
+        var listFavoutiteMeals = this.state.listFavoutiteMeals.concat(this.state.newFavouriteMeals)
         this.setState({
-            listDontLike: listDontLike
+            listFavoutiteMeals: listFavoutiteMeals
         });
-        this.newdontLikehandler();
-        //      console.log(this.state.listDontLike)
+        this.newFavouriteMealshandler();
+        //      console.log(this.state.listFavoutiteMeals)
     }
 
     onClickDelIngredient = (reactKey) => {
-        var lis = this.state.listDontLike;
-        lis = this.state.listDontLike.filter((it, indexitem) => {
+        var lis = this.state.listFavoutiteMeals;
+        lis = this.state.listFavoutiteMeals.filter((it, indexitem) => {
             return indexitem !== Number(reactKey.target.value);
         });
         this.setState({
-            listDontLike: lis
+            listFavoutiteMeals: lis
         })
-        this.newdontLikehandler();
+        this.newFavouriteMealshandler();
         console.log(lis)
     }
 
-    newdontLikehandler() {
+    newFavouriteMealshandler() {
         //grab the user  info 
         const storeRef = base.database().ref(getUserLoginData().uid);
         console.log('test ' + storeRef)
         // query the firebase
         storeRef.once('value', (snapshot) => {
             const data = snapshot.val() || {};
+            console.log(data)
             //Add some data to the user...
             storeRef.child('restricitons').update({
-                dontlike: this.state.listDontLike
+                favouriteMeals: this.state.listFavoutiteMeals
             })
 
         });
@@ -106,11 +108,11 @@ class DontLike extends React.Component {
 componentWillMount()
 {
         const storeRef = base.database().ref(getUserLoginData().uid);
-        storeRef.child('restricitons').once('value', (snapshot) => {
+       storeRef.child('restricitons').once('value', (snapshot) => {
             const data = snapshot.val() || {};
-            if (data.dontlike) {
+            if (data.favouriteMeals) {
                 this.setState({
-                   listDontLike : data.dontlike
+                   listFavoutiteMeals : data.favouriteMeals
                 })
             }
         });
@@ -120,9 +122,9 @@ componentWillMount()
         return (
             <div className='card'>
                 <div className='card-block'>
-                    <h1 className="card-title">I Don't Like</h1>
+                    <h1 className="card-title">My Favourite Meals</h1>
                     <IngredientInput onChangIngrdientInput={this.onChangIngrdientInput} onClickAddIngredient={this.onClickAddIngredient} />
-                    <IgredientListComp listDontLike={this.state.listDontLike} onClickDelIngredient={this.onClickDelIngredient} />
+                    <IgredientListComp listFavoutiteMeals={this.state.listFavoutiteMeals} onClickDelIngredient={this.onClickDelIngredient} />
                     <SaveButton />
                 </div>
             </div>
@@ -130,4 +132,4 @@ componentWillMount()
     }
 }
 
-export default DontLike;
+export default FavouriteMeals;
