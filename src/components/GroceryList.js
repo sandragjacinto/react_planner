a9cbf68c387f76e02258b27508e98a99e7972e40
Smouska <sandragjacinto@gmail.com>
2017-08-12 09:31:41 -0400

    commits grocery list whithout error

--- src/components/GroceryList.js
+++ src/components/GroceryList.js
@@ -32,7 +32,7 @@ class GroceryList extends React.Component {
         this.state = {
             tempgrocerieList: [],
             grocerieList: [],
-            newtemp : [],
+            newtemp: [],
         }
 
         const storeRef = base.database().ref(getUserLoginData().uid);
@@ -40,41 +40,54 @@ class GroceryList extends React.Component {
             const data = snapshot.val() || {};
             if (data) {
                 for (var i in data) {
-                    var grocerieLists = this.state.tempgrocerieList.concat(data[i].ingredientLines)
-                    
-                    //console.log(grocerieLists)
+                    var tempList = data[i].ingredientLines;
+                    var ll = tempList.length
+                    console.log(tempList.length)
+                    for (var ing in tempList) {
+                        var grocerieLists = this.state.tempgrocerieList.push(tempList[ing])
+
+                    }//
+
                 }
             }
-           this.setState({
-                grocerieList: grocerieLists,
-            }) 
-
-            for (var i in (this.state.grocerieList)) {
-                    console.log('test :' + this.state.grocerieList[i])
-                }
-
-                
-
-           // console.log(this.state.grocerieList)
+            //console.log (this.state.tempgrocerieList)
+            this.setState({
+                grocerieList: this.state.tempgrocerieList,
+            })
+            // console.log(this.state.grocerieList)
         });
 
     }
 
-    
-        render() {
-            
-            return (
-                <div style={{ textAlign: "center" }}>
-                    <div className="row">
-                        <div className="col-md-11 col-xs-12" style={{ paddingTop: '30px', }}>
-                            <h2>Working in progress...</h2>
-                            <p>{this.state.grocerieList[0]}</p>
-                        </div>
+
+    render() {
+
+        return (
+            <div style={{ textAlign: "center" }}>
+                <div className="row">
+                    <div className="col-md-11 col-xs-12" style={{ paddingTop: '30px', }}>
+                        <h2>Grocerie List</h2>
+                        {this.state.grocerieList.map((item, index) => {
+                            return (
+                                <div className='row list-group-item '>
+                                    <div className='col-md-9 col-xs-9'>
+                                        <h4 key={index}>
+                                            {item}
+                                        </h4>
+                                    </div>
+                                    <div className='col-md-1 col-md-offset-2 col-xs-1 col-xs-offset-2' >
+                                        <button key={index} className="btn btn-danger" value={index} type="button" >x</button>
+                                    </div>
+                                </div>
+                            )
+                        }
+                        )}
                     </div>
                 </div>
-            )
-        }
-        
+            </div>
+        )
+    }
+
 }
 
 export default GroceryList;
